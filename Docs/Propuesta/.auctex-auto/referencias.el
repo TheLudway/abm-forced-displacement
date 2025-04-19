2ce955ace486d4d962a0557f87ab738edf04ed46
TheLudway <theludway@gmail.com>
2025-04-19 17:20:01 -0500

    Contexto y descripción del problema

---
+++ Docs/Propuesta/.auctex-auto/referencias.el
@@ -0,0 +1,21 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "referencias"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "suleimenova2020predicting"
+    "hebert2017agent"
+    "Gutierrez2012"
+    "tovar2023modelacion"
+    "santacoloma2015importancia"
+    "perez2016impacto"
+    "ruiz2011desplazamiento"
+    "bello2003desplazamiento"
+    "villa2006desplazamiento"
+    "unidadvictimas_ruv_2025"
+    "eswiki:165709323"
+    "eswiki:165573820"
+    "solano2020determinantes"))
+ '(or :bibtex :latex))
+
